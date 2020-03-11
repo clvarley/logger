76def7ae332f71746d563ee0d048e37a9792a783
clvarley <conorlukevarley@gmail.com>
2020-03-11 10:34:26 +0000

    Created a /dev/null type logger

---
+++ NullLogger.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Logging;
+
+use \Logging\LoggerInterface;
+
+/**
+ * Provides a void into which messages can be thrown
+ *
+ * @author <clvarley>
+ */
+Class NullLogger Implements LoggerInterface
+{
+
+    /**
+     * Discards the given message
+     *
+     * @param string $message Log message
+     * @return void           N/a
+     */
+    public function log( string $message ) : void
+    {
+        return;
+    }
+
+    /**
+     * Discards the given informational message
+     *
+     * @param string $message Log message
+     * @return void           N/a
+     */
+    public function info( string $message ) : void
+    {
+        return;
+    }
+
+    /**
+     * Discards the given warning message
+     *
+     * @param string $message Log message
+     * @return void           N/a
+     */
+    public function warn( string $message ) : void
+    {
+        return;
+    }
+
+    /**
+     * Discards the given error message
+     *
+     * @param string $message Log message
+     * @return void           N/a
+     */
+    public function error( string $message ) : void
+    {
+        return;
+    }
+
+    /**
+     * Discards the given message
+     *
+     * @param int    $level   Log level
+     * @param string $message Log message
+     * @return void           N/a
+     */
+    public function write( int $level, string $message ) : void
+    {
+        return;
+    }
+}
