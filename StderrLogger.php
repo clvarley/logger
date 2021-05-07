3c9a999b0c0feb4368af36d17a806916ef6e42f7
clvarley <conorlukevarley@gmail.com>
2021-05-07 22:24:12 +0100

    Changed generic console to STDERR logger

--- StderrLogger.php
+++ StderrLogger.php
@@ -4,10 +4,16 @@ namespace Logging;
 
 use Logging\LoggerInterface;
 
+use function fwrite;
+
+use const STDERR;
 use const PHP_EOL;
 
 /**
- * Provides functions to log to the console
+ * Provides functions to log directly to STDERR
+ *
+ * As STDERR has no concept of log levels, all of these functions cause
+ * identical behaviour.
  *
  * @author clvarley
  */
@@ -15,59 +21,51 @@ Class ConsoleLogger Implements LoggerInterface
 {
 
     /**
-     * Write a standard message to the console
+     * Write a standard message to stderr
      *
      * @param string $message Log message
      * @return void           N/a
      */
     public function log( string $message ) : void
     {
-        echo $message . PHP_EOL;
-
-        return;
+        fwrite( STDERR, $message . PHP_EOL );
     }
 
     /**
-     * Write an informational message to the console
+     * Write an informational message to stderr
      *
      * @param string $message Log message
      * @return void           N/a
      */
     public function info( string $message ) : void
     {
-        echo $message . PHP_EOL;
-
-        return;
+        fwrite( STDERR, $message . PHP_EOL );
     }
 
     /**
-     * Write a warning message to the console
+     * Write a warning message to stderr
      *
      * @param string $message Log message
      * @return void           N/a
      */
     public function warn( string $message ) : void
     {
-        echo $message . PHP_EOL;
-
-        return;
+        fwrite( STDERR, $message . PHP_EOL );
     }
 
     /**
-     * Write an error message to the console
+     * Write an error message to stderr
      *
      * @param string $message Log message
      * @return void           N/a
      */
     public function error( string $message ) : void
     {
-        echo $message . PHP_EOL;
-
-        return;
+        fwrite( STDERR, $message . PHP_EOL );
     }
 
     /**
-     * Writes a message to the console using the given log level
+     * Writes a message to stderr using the given log level
      *
      * @param int    $level   Log level
      * @param string $message Log message
@@ -75,8 +73,6 @@ Class ConsoleLogger Implements LoggerInterface
      */
     public function write( int $level, string $message ) : void
     {
-        echo $message . PHP_EOL;
-
-        return;
+        fwrite( STDERR, $message . PHP_EOL );
     }
 }
